5a64013f71b81f8415e4f412c036f1aab4df0368
Mdlove59 <benjii280@gmail.com>
2025-06-08 21:05:38 +0200

    Add files via upload

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2025 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/twrp_agate.mk
+
+COMMON_LUNCH_CHOICES := \
+    twrp_agate-user \
+    twrp_agate-userdebug \
+    twrp_agate-eng
